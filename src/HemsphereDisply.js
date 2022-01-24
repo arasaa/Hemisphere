293346e8506c3d1fb965e109edf95cbdfee24afd
araz saydu <arazsaydu@gmail.com>
2022-01-24 08:03:28 +0100

    complete

--- src/HemsphereDisply.js
+++ src/HemsphereDisply.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import './style.css';
+import northPic from './image/north.png'
+import southPic from './image/south.jpg'
 
-const HemsphereDisply = () => {
+
+const hemisphereConfig = {
+    northern: {
+        text: 'it is norhtern hemisphere',
+        picture: northPic
+    },
+    southern: {
+        text: 'it is south hemisphere',
+        picture: southPic
+    }
+}
+const HemsphereDisply = ({latitude}) => {
+
+    const hemisphere = latitude > 0? 'northern' : 'southern';
+    const {text, picture} = hemisphereConfig[hemisphere];
     return(
-        <div>northen hemes</div>
+        <div className={hemisphere}>
+            <div className='ui raised text container segment'>
+                <div className='image'>
+                <img src={picture} alt='pic' />
+                </div>
+                <div className='ui teal bottom attached label'>
+                   <h1>{text}</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import HemsphereDisply from './HemsphereDisply'
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {latitude: null, errorMessage: ''}
+    state = {latitude: null, errorMessage: ''}
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({latitude: position.coords.latitude})
@@ -26,7 +24,7 @@ class App extends React.Component {
             return <div>{this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.latitude){
-            return <div>{this.state.latitude}</div>
+            return <div><HemsphereDisply latitude = {this.state.latitude}/></div>
         }
         else{
             return <div>Loading..</div>
